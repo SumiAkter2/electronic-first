74f3b4205b04c953fe90fe42ef66cb8dcaac11e5
SumiAkter2 <akteralisha36@gmail.com>
2023-08-19 14:44:25 +0600

    added dropdown in main.js

--- main.js
+++ main.js
@@ -1,8 +1,108 @@
 // console.log("from  main.js .dddddddddddddddddddd");
-// console.log("Allah Help me please ");
+console.log("Allah Help me please ");
+// hide nav and display input field;
+function hideElement() {
+  const elementToHide = document.getElementById("main-nav-items");
+  elementToHide.style.display = "none";
+  const currentDisplayStyle = elementToHide.style.display;
+  if (currentDisplayStyle === "none") {
+    elementToHide.style.display = "none";
+  }
+  const section = document.getElementById("search-container");
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div class="search-content">
+    <div class="search">
+    <input type="text" class="search__input" placeholder="Minecraft, RPG, multiplayer... ">
+    <buttons>Advanced Search</buttons>
+    </div>
+    <buttons id="x-button" class="x-buttons">
+    <i class="fa-solid fa-x"></i>
+    </button>
+    </div>
+    `;
+  section.appendChild(div);
+}
+const hideButton = document.getElementById("hide-button");
+hideButton.addEventListener("click", hideElement);
 
+// added dropdown
+let isElementAdded = false;
+function addDropdown() {
+  console.log("add");
+  const section = document.getElementById("dropdown-contents");
+  const div = document.createElement("div");
+  div.innerHTML = `
+                          <div>
+                          <div class="dropdown-header">
+                              <h4>Platform </h4>
+                              
+                             <p>view all</p>
+                            </div>
+                            <div class="dropdown-item">
+                              <p>
+                                <img src="/public/img/icon/steam.png" alt="icon-logo"> Steam
+                              </p>
+                            <p>
+                                <img src="/public/img/icon/ubisoft.png" alt="icon-logo"> Ubisoft Connect
+                              </p>
+                                <p>
+                                <img src="/public/img/icon/ea.png" alt="icon-logo"> EA App
+                              </p>
+                              <p>
+                                <img src="/public/img/icon/battle.jpg" alt="icon-logo"> Battle.net
+                              </p>
+                              <p>
+                                <img src="/public/img/icon/rockstar.png" alt="icon-logo">RockStar
+                              </p> 
+                              <p>
+                                <img src="/public/img/icon/gog.png" alt="icon-logo"> GOG.com
+                              </p>
+                              <p>
+                                <img src="/public/img/icon/Microsoft_logo.svg.png" alt="icon-logo"> Microsoft Store
+                              </p>
+                              <p>
+                                <img src="/public/img/icon/epic.png" alt="icon-logo"> Epic
+                              </p>
+                            </div>
+                          
+                            <div class="dropdown-header">
+                              <h4>Prepaid Cards </h4>
+                              <p>View all</p>
+                             
+                            </div>
+                            <div class="dropdown-bottom-item">
+                              <p>
+                                <img src="/public/img/icon/steam.png" alt="icon-logo"> Steam Gift Cards
+                              </p>
+                              <p>
+                                <img src="/public/img/icon/download-removebg-preview.png" alt="icon-logo"> IG Gift Cards
+                              </p>
+                              <p>
+                                <img src="/public/img/icon/blizzard-removebg-preview.png" alt="icon-logo" style="width: 80px;"> Blizzard Gift
+                                Cards
+                              </p>
+                            </div>
+                          
+                          
+                          </div> 
+                          </div>
 
+  `;
+  section.appendChild(div);
+}
+const addButton = document.getElementById("add-button");
+addButton.addEventListener("click", addDropdown);
 
+// function SearchButtonBack() {
+//   console.log("back");
+//   const inputField = document.getElementById("search-container");
+//   inputField.style.display = none;
+//   const backElement = document.getElementById("main-nav-items");
+//   backElement.style.display = block;
+// }
+// const backElements = document.getElementById("x-button");
+// backElements.addEventListener("click", SearchButtonBack);
 
 // fetch product form product.json
 function loadData() {
